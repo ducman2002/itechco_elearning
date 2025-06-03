3ffd915900f99c3c2a8ea840ad51473dd1846fb7
ducman2002 <man.hasagi020@gmail.com>
2025-06-03 13:35:25 +0700

    Initial commit

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,16 +1,19 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import { useNavigate } from "react-router-dom";
 
 import Logo from "~/resources/Images/Logo.png";
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const navigate = useNavigate();
+
     return (
         <div className={cx("wrapper")}>
             <div className={cx("inner")}>
                 <div className={cx("logo")}>
-                    <img src={Logo} alt="Logo" />
+                    <img className={cx("image-logo")} src={Logo} alt="Logo" onClick={() => navigate("/")} />
                 </div>
                 <div className={cx("search-bar")}>
                     <input className={cx("search-box")} placeholder="Search..." spellCheck={false} />
--- src/components/Layouts/DefaultLayout/Slidebar/index.js
+++ src/components/Layouts/DefaultLayout/Slidebar/index.js
@@ -1,15 +1,56 @@
 import classNames from "classnames/bind";
+import { useNavigate } from "react-router-dom";
 import styles from "./Slidebar.module.scss";
+import { useState } from "react";
 
 const cx = classNames.bind(styles);
 
+const menuItems = [
+    { label: "Tương Tác Game", path: "/exercise" },
+    { label: "Khóa Học", path: "/course" },
+];
+
+const courses = ["Khóa học 1", "Khóa học 2", "Khóa học 3", "Khóa học 4"];
+
 function Slidebar() {
+    const navigate = useNavigate();
+    const [selectedMenu, setSelectedMenu] = useState(null);
+    const [showCourses, setShowCourses] = useState(false);
+
+    const handleMenuClick = (idx, path) => {
+        if (selectedMenu === idx) {
+            setSelectedMenu(null);
+            if (idx === 1) setShowCourses(false);
+        } else {
+            setSelectedMenu(idx);
+            navigate(path);
+            if (idx === 1) setShowCourses(true);
+            else setShowCourses(false);
+        }
+    };
+
     return (
         <div className={cx("wrapper")}>
             <div className={cx("inner")}>
                 <div className={cx("menu")}>
-                    <div className={cx("menu-item")}>Tương Tác Game</div>
-                    <div className={cx("menu-item")}>Khóa Học</div>
+                    {menuItems.map((item, idx) => (
+                        <div
+                            key={item.label}
+                            className={cx("menu-item", { selected: selectedMenu === idx })}
+                            onClick={() => handleMenuClick(idx, item.path)}
+                        >
+                            {item.label}
+                        </div>
+                    ))}
+                    {showCourses && (
+                        <div className={cx("list-item-course")}>
+                            {courses.map((course, i) => (
+                                <div key={i} className={cx("item-course-content-item")}>
+                                    {course}
+                                </div>
+                            ))}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
